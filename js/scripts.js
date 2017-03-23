21459997690a0860d8ed55860a70d9e1290b1b6c
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-23 23:58:48 +0100

    adding a value of 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
  // plik scripts.js
 
-var a = 15,
-	b = 7,
-	value = (a * a) + (2 * a * b) - (b * b);
-console.log('Wasrtość dodawania liczby a podniesionej do kwadratu' + a + ' i liczbt b podniesionej do kwadratu: ' + b + ' wynosi: ' + value);
-if ( value >= 0) {
+var a = prompt('Wpisz wartość liczby a');
+var	b = prompt('Wpisz wartość liczby b');
+var value = ((a * a) + (2 * a * b) - (b * b));
+console.log("Wasrtość dodawania liczby a=" + a + " podniesionej do kwadratu i liczbt b=" + b + " podniesionej do kwadratu wynosi: " + value);
+if (value > 0) {
 	console.log("Wartość dodatnia");
-} if ( value < 0) {
+} if (value < 0) {
     console.log("Wartość ujemna");
+} if (value == 0) {
+    console.log("0");
 }
