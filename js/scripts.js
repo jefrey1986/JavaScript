d7ab6230e66f5d762e51d22794827f516fe995bd
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-23 17:13:36 +0100

    new mathematic conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
- 
+ // plik scripts.js
+
+var a = 15,
+	b = 7,
+	value = (a * a) + (2 * a * b) - (b * b);
+console.log('Wasrtość dodawania liczby a podniesionej do kwadratu' + a + ' i liczbt b podniesionej do kwadratu: ' + b + ' wynosi: ' + value);
+if ( value >= 0) {
+	console.log("Wartość dodatnia");
+} if ( value < 0) {
+    console.log("Wartość ujemna");
+}
