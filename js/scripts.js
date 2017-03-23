216956b783bfea3df523268a73779374e3d5a250
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-23 12:22:44 +0100

    trinagle area

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
- 
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imię');
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+
+var a = prompt('Wpisz długość podstawy trójkąta a')
+	h = prompt('Wpisz wysokość trójkąta h')
+	triangleArea = a*h/2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
