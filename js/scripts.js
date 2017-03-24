b305d9060a014958d97d6c18c5858ff6cd78561c
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-24 01:45:10 +0100

    Trinagle area

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
- 
+var a = prompt('Wpisz wartość liczby a');
+var h = prompt('Wpisz wartość liczby h');
+function getTriangleArea(a, h) {
+} 
+if ( (a <= 0) || (h <= 0) ) {
+	console.log("Nieprawidłowe dane");
+} if ( (a > 0) && (h > 0) ) {
+	function getTriangleArea(a, h) {
+	var result = a*h/2
+	return result
+	} 
+	console.log( getTriangleArea(10,6) )
+}
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(8, 10);
+var triangle6Area = getTriangleArea(12, 22);
