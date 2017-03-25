ef8e7ac8d55fdb48cf8b875c683751dac619fd54
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-25 15:57:04 +0100

    change conditions

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,8 @@ var value = ((a * a) + (2 * a * b) - (b * b));
 console.log("Wasrtość dodawania liczby a=" + a + " podniesionej do kwadratu i liczbt b=" + b + " podniesionej do kwadratu wynosi: " + value);
 if (value > 0) {
 	console.log("Wartość dodatnia");
-} if (value < 0) {
-    console.log("Wartość ujemna");
-} if (value == 0) {
-    console.log("0");
+} else if (value < 0) {
+	console.log("Wartość ujemna");
+} else (value == 0) {
+	console.log("0");
 }
