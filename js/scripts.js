673cd8edbac1c5eadc042ee8200240de4aa0940a
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-25 15:36:36 +0100

    var

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
 
-var a = prompt('Wpisz długość podstawy trójkąta a')
-	h = prompt('Wpisz wysokość trójkąta h')
-	triangleArea = a*h/2;
+var a = prompt('Wpisz długość podstawy trójkąta a');
+var h = prompt('Wpisz wysokość trójkąta h');
+var triangleArea = a*h/2;
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
