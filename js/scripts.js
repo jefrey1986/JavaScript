d0ce24ef221485264deddc55a348cd42a0be3729
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-26 21:21:09 +0200

    Green elephants

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
- 
+ var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
+ var animal = "Zielone słonie";
+ var animalUpperCased = animal.toUpperCase();
+ var textAfter = text.replace('Papugi', animalUpperCased);
+ console.log(textAfter.length/2);
+ var textEnd = textAfter.slice(0, 45);
+ console.log(textEnd);
