a118ca25bf9fdb374587031cf6d5bb57c296df0c
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-27 19:28:37 +0200

    christmas tree

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
- 
+function rysujChoinke(n) {
+	for ( var k = 1 ; k < n; k++) {
+		var star = '*';
+			for ( var j = 1 ; j < k*2-1; j++) {
+				star  += '*';
+			}
+			console.log(star);
+	}
+	
+} 
+rysujChoinke(6);
