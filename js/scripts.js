7d033b615fa00c1167db4bff505647ce2376a588
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-27 16:01:00 +0200

    update if

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@
 var a = prompt('Wpisz wartość liczby a');
 var	b = prompt('Wpisz wartość liczby b');
 var value = ((a * a) + (2 * a * b) - (b * b));
-console.log("Wasrtość dodawania liczby a=" + a + " podniesionej do kwadratu i liczbt b=" + b + " podniesionej do kwadratu wynosi: " + value);
+console.log("Wartość dodawania liczby a=" + a + " podniesionej do kwadratu i liczbt b=" + b + " podniesionej do kwadratu wynosi: " + value);
 if (value > 0) {
 	console.log("Wartość dodatnia");
 } else if (value < 0) {
 	console.log("Wartość ujemna");
-} else (value == 0) {
+} else {
 	console.log("0");
 }
