934b2a3c205f612537a31195f2c66653ac01cf1d
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-27 16:22:37 +0200

    new variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
-var a = prompt('Wpisz wartość liczby a');
-var h = prompt('Wpisz wartość liczby h');
-
 function getTriangleArea(a, h) {
 	if ( (a <= 0) || (h <= 0) ) {
 		console.log("Nieprawidłowe dane");
-	} else ( (a > 0) && (h > 0) ) {
+	} else {
 		var result = a*h/2
 		return result
-		console.log( getTriangleArea(10,6) )
 	}
 }
+console.log( getTriangleArea(10,6) )
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(8, 10);
 var triangle3Area = getTriangleArea(12, 22);
+console.log( triangle1Area, triangle2Area, triangle3Area )
