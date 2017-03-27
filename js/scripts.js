60bf5b32c80e522ca5c9b0ddd720d660bf6b7a85
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-27 22:54:44 +0200

    button

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
- 
+ var withButtonClass = document.getElementsByClassName("button");
+ console.log(withButtonClass);
+ var btn = withButtonClass.length;
+ console.log(btn);
+for (k = 0; k < btn; k++) {
+	alert (withButtonClass[k].innerText);
+}
+
