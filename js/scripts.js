a8a806851618604c613b89f064ec325de7a00bfa
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-28 13:21:08 +0200

    new text variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
  var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
  var animal = "Zielone słonie";
  var animalUpperCased = animal.toUpperCase();
- var textAfter = text.replace('Papugi', animalUpperCased);
+ var FirstWordOfText = text.split(' ', 1);
+ var textAfter = text.replace(FirstWordOfText, animalUpperCased);
  var textEnd = textAfter.slice(0, textAfter.length/2);
  console.log(textEnd);
