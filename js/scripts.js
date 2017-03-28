40d9e710d4847cb0717d48a24b8b2c7c2286bc9d
Mateusz Mulcan <jefrey1986@o2.pl>
2017-03-28 15:50:02 +0200

    new point - event

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
- 
+var list = document.getElementById("list");
+var add = document.getElementById('addElem');
+var newPoint = document.getElementsByTagName('li');
+add.addEventListener('click', function() {
+	var number = newPoint.length;
+	list.innerHTML += "<li>item " + number + "</li>";
+	console.log();
+});
+
